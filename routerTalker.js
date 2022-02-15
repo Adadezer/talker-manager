6559d86212622b976d9202e4ca7c865f3a06ccd1
Adadezer <adadezer@gmail.com>
2022-02-15 16:11:04 -0300

    requisito 2, feito

--- routerTalker.js
+++ routerTalker.js
@@ -10,4 +10,16 @@ router.get('/', (req, resp) => {
   return resp.status(200).send(JSON.parse(palestrantes));
 });
 
+router.get('/:id', (req, resp) => {
+  const palestrantes = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
+  const { id } = req.params;
+
+  const findPalestrantes = palestrantes.find((p) => p.id === Number(id));
+
+  if (!findPalestrantes) {
+    return resp.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return resp.status(200).json(findPalestrantes);
+});
 module.exports = router;
